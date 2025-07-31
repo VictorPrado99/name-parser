0c5fe5954f303f0b3228e80ae27007685c7fc741
Victor Prado <vxavierprado@gmail.com>
2025-07-31 19:11:10 +0100

    wip: basic implementation using an external library

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-}
+
+    // We could use this dependency to really sped up things, not needing to implement the algorithm ourselves. For demonstration purposes, we won't
+    implementation("org.ahocorasick:ahocorasick:0.4.0")}
 
 tasks.test {
     useJUnitPlatform()
